428e5a08a369aec3735127ca795c8d73300228c3
peacepassion <daqi1990@gmail.com>
2015-12-08 21:24:52 +0800

    change config to key-value style

--- pjs.py
+++ pjs.py
@@ -6,8 +6,9 @@ import optparse
 import json
 import BaseHTTPServer
 import SocketServer
-
 import json_utils
+import ConfigParser
+import StringIO
 
 
 def usage():
@@ -18,7 +19,14 @@ def usage():
 CONFIG_KEY_HOST = 'host'
 CONFIG_KEY_PORT = 'port'
 CONFIG_KEY_RESPONSE_CODE = 'response_code'
-DEFAULT_CONFIG = {CONFIG_KEY_HOST: 'localhost', CONFIG_KEY_PORT: 3000, CONFIG_KEY_RESPONSE_CODE: 200}
+
+DEFAULT_CONFIG_HOST = 'localhost'
+DEFAULT_CONFIG_PORT = 3000
+DEFAULT_CONFIG_RESPONSE_CODE = 200
+
+host = DEFAULT_CONFIG_HOST
+port = DEFAULT_CONFIG_PORT
+response_code = DEFAULT_CONFIG_RESPONSE_CODE
 
 option_list = [optparse.make_option('-c', dest='config', default=''),
                optparse.make_option('-v', dest='verbose', action='store_true', default=False,
@@ -41,21 +49,31 @@ if len(args) > 1:
     exit(1)
 
 
-config = DEFAULT_CONFIG
 if opt.config != '':
-    config_file = open(opt.config)
     try:
-        user_config = json.load(config_file)
-        if CONFIG_KEY_HOST in user_config:
-            config[CONFIG_KEY_HOST] = user_config[CONFIG_KEY_HOST]
-        if CONFIG_KEY_PORT in user_config:
-            config[CONFIG_KEY_PORT] = user_config[CONFIG_KEY_PORT]
-        if CONFIG_KEY_RESPONSE_CODE in user_config:
-            config[CONFIG_KEY_RESPONSE_CODE] = user_config[CONFIG_KEY_RESPONSE_CODE]
+        org_config_file = open(opt.config)
+        org_config = org_config_file.read()
+        fake_section = 'section'
+        org_config = '[' + fake_section + ']' + '\n' + org_config
+        cf = ConfigParser.RawConfigParser()
+        cf.readfp(StringIO.StringIO(org_config))
+
+        if cf.has_option(fake_section, CONFIG_KEY_HOST):
+            host = cf.get(fake_section, CONFIG_KEY_HOST)
+        if cf.has_option(fake_section, CONFIG_KEY_PORT):
+            port = cf.getint(fake_section, CONFIG_KEY_PORT)
+        if cf.has_option(fake_section, CONFIG_KEY_RESPONSE_CODE):
+            response_code = cf.getint(fake_section, CONFIG_KEY_RESPONSE_CODE)
+
     finally:
-        config_file.close()
+        org_config_file.close()
+
+
 if opt.verbose is True:
-    print 'config: ' + str(config)
+    print 'config as below: '
+    print 'host: ' + str(host)
+    print 'port: ' + str(port)
+    print 'response_code: ' + str(response_code)
 
 try:
     response_file = open(args[0])
@@ -72,7 +90,7 @@ if opt.verbose:
 class JsonServerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
         self._log_request()
-        self.send_response(config[CONFIG_KEY_RESPONSE_CODE])
+        self.send_response(response_code)
         self.send_header("Content-Type", "application/json")
         self.end_headers()
         try:
@@ -90,5 +108,5 @@ class JsonServerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         print str(self.command) + SEP + str(self.client_address) + SEP + str(self.path)
 
 
-httpd = SocketServer.TCPServer((config[CONFIG_KEY_HOST], config[CONFIG_KEY_PORT]), JsonServerHandler)
+httpd = SocketServer.TCPServer((host, port), JsonServerHandler)
 httpd.serve_forever()
