e4812f660c67835d8cd390ec7876c5df14d172eb
peacepassion <daqi1990@gmail.com>
2015-12-19 20:16:37 +0800

    normalize some name convention and basic syntax

--- config_parser.py
+++ config_parser.py
@@ -6,12 +6,6 @@ import optparse
 
 __author__ = 'peacepassion'
 
-
-def usage():
-    print 'usage: cmd [-c config-file] [-v] json-data'
-    exit(1)
-
-
 CONFIG_KEY_HOST = 'host'
 CONFIG_KEY_PORT = 'port'
 CONFIG_KEY_RESPONSE_FILE = 'response_file'
@@ -22,7 +16,12 @@ DEFAULT_CONFIG_PORT = 3000
 DEFAULT_CONFIG_RESPONSE_CODE = 200
 
 
-def validate_response_file(file_path):
+def _usage():
+    print 'usage: cmd [-c config-file] [-v] json-data'
+    exit(1)
+
+
+def _validate_response_file(file_path):
     _response_file = open(file_path)
 
     try:
@@ -64,41 +63,41 @@ class MyConfigUnit:
 
 
 class MyConfigParser:
-    __global_config__ = None
-    __section_configs__ = {}
-    __fake_global_section__ = '__global_section__'
+    __global_config = None
+    __section_configs = {}
+    __fake_global_section = '__global_section__'
 
     def __init__(self):
-        self.__parse_cmd__()
-        self.__parse_config_file__()
+        self.__parse_cmd()
+        self.__parse_config_file()
 
-    def __parse_cmd__(self):
+    def __parse_cmd(self):
         option_list = [optparse.make_option('-v', dest='verbose', action='store_true', default=False,
                                             help='print all information')]
         parser = optparse.OptionParser(usage='Usage: %prog [-v] config-file', option_list=option_list)
-        self.opt, self.args = parser.parse_args()
+        self.__opt, self.__args = parser.parse_args()
 
-        if self.opt.verbose is True:
-            print 'opt: ' + str(self.opt)
-            print 'args: ' + str(self.args)
-        if len(self.args) < 1:
+        if self.__opt.verbose:
+            print 'opt: %s' % str(self.__opt)
+            print 'args: %s' % str(self.__args)
+        if self.__args is False:  # judge if list's length does not equals zero
             print 'fail to find config-file'
             exit(1)
 
-        self.__config_file__ = self.args[0]
+        self.__config_file = self.__args[0]
 
     def is_verbose(self):
-        return self.opt.verbose
+        return self.__opt.verbose
 
-    def __parse_config_file__(self):
-        self.__global_config__ = GlobalConfig()
+    def __parse_config_file(self):
+        self.__global_config = GlobalConfig()
 
-        if self.__config_file__ != '':
-            org_config_file = open(self.__config_file__)
+        if self.__config_file:  # '' means False in python
+            org_config_file = open(self.__config_file)
 
             try:
                 org_config = org_config_file.read()
-                org_config = '[' + self.__fake_global_section__ + ']' + '\n' + org_config
+                org_config = '[' + self.__fake_global_section + ']' + '\n' + org_config
                 cf = ConfigParser.RawConfigParser()
                 cf.readfp(StringIO.StringIO(org_config))
 
@@ -109,18 +108,18 @@ class MyConfigParser:
                 org_config_file.close()
 
     def __parse_global_config__(self, cf):
-        if cf.has_option(self.__fake_global_section__, CONFIG_KEY_HOST):
-            self.__global_host__.host = cf.get(self.__fake_global_section__, CONFIG_KEY_HOST)
-        if cf.has_option(self.__fake_global_section__, CONFIG_KEY_PORT):
-            self.__global_config__.port = cf.getint(self.__fake_global_section__, CONFIG_KEY_PORT)
-        if cf.has_option(self.__fake_global_section__, CONFIG_KEY_RESPONSE_CODE):
-            self.__global_config__.response_code = cf.getint(self.__fake_global_section__, CONFIG_KEY_RESPONSE_CODE)
+        if cf.has_option(self.__fake_global_section, CONFIG_KEY_HOST):
+            self.__global_config.host = cf.get(self.__fake_global_section, CONFIG_KEY_HOST)
+        if cf.has_option(self.__fake_global_section, CONFIG_KEY_PORT):
+            self.__global_config.port = cf.getint(self.__fake_global_section, CONFIG_KEY_PORT)
+        if cf.has_option(self.__fake_global_section, CONFIG_KEY_RESPONSE_CODE):
+            self.__global_config.response_code = cf.getint(self.__fake_global_section, CONFIG_KEY_RESPONSE_CODE)
 
     def __parse_each_section__(self, cf):
         section_list = cf.sections()
 
         for section in section_list:
-            if section == self.__fake_global_section__:
+            if section == self.__fake_global_section:
                 continue
 
             config_unit = MyConfigUnit()
@@ -129,23 +128,36 @@ class MyConfigParser:
             options = cf.options(section)
 
             if CONFIG_KEY_RESPONSE_FILE not in options:
-                print "fail to find response_file configuration under section " + section
+                print "fail to find response_file configuration under section %s" % section
                 exit(1)
             response_file_path = cf.get(section, CONFIG_KEY_RESPONSE_FILE)
-            if not validate_response_file(response_file_path):
-                print 'response_file: ' + response_file_path + ' is an illegal json string'
+            if not _validate_response_file(response_file_path):
+                print 'response_file: %s is an illegal json string' % response_file_path
                 exit(1)
             config_unit.response_file = response_file_path
 
-            if CONFIG_KEY_RESPONSE_CODE not in options:
-                config_unit.response_code = DEFAULT_CONFIG_RESPONSE_CODE
-            else:
-                config_unit.response_code = cf.getint(section, CONFIG_KEY_RESPONSE_CODE)
+            config_unit.response_code = cf.getint(section, CONFIG_KEY_RESPONSE_CODE) \
+                if CONFIG_KEY_RESPONSE_CODE in options \
+                else DEFAULT_CONFIG_RESPONSE_CODE
 
-            self.__section_configs__[config_unit.path_key] = config_unit
+            self.__section_configs[config_unit.path_key] = config_unit
 
     def global_config(self):
-        return self.__global_config__
+        return self.__global_config
 
     def section_configs(self):
-        return self.__section_configs__
+        return self.__section_configs
+
+
+if __name__ == '__main__':
+    print 'test config_parser usage'
+    cfg = MyConfigParser()
+
+    if cfg.is_verbose():
+        print '[global config]'
+        print cfg.global_config()
+
+        print '[each section]'
+        section_cfgs = cfg.section_configs()
+        for key in section_cfgs:
+            print '----%s \n%s' % (key, section_cfgs[key])
--- pjs.py
+++ pjs.py
@@ -1,44 +1,60 @@
 # /usr/bin/python
 
-__author__ = 'peacepassion'
-
 import BaseHTTPServer
 import SocketServer
-
 import config_parser
 
-cfg = config_parser.MyConfigParser()
+__author__ = 'peacepassion'
 
-if cfg.is_verbose():
-    print '[global config]'
-    print cfg.global_config()
 
-    print '[each section]'
-    section_cfgs = cfg.section_configs()
-    for key in section_cfgs.keys():
-        print key
-        print section_cfgs[key]
+def _read_config():
+    cfg = config_parser.MyConfigParser()
+
+    if cfg.is_verbose():
+        print '[global config]'
+        print cfg.global_config()
+
+        print '[each section]'
+        section_cfgs = cfg.section_configs()
+        for key in section_cfgs:
+            print '----%s \n%s' % (key, section_cfgs[key])
+
+    return cfg
+
+
+def main():
+    cfg = _read_config()
+    _start_server(cfg)
+    return 0
+
+
+def _start_server(_cfg):
+    JsonServerHandler.cfg = _cfg
+    httpd = SocketServer.TCPServer((_cfg.global_config().host, _cfg.global_config().port), JsonServerHandler)
+    httpd.serve_forever()
 
 
 class JsonServerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    def do_GET(self):
-        self._log_request()
+    cfg = None
 
-        code, body = self.__build_response__()
+    def do_GET(self):
+        self.__log_request()
+
+        code, body = self.__build_response()
         self.send_response(code)
         self.send_header("Content-Type", "application/json")
         self.end_headers()
 
         self.wfile.write(body)
 
-    def __build_response__(self):
-        body = cfg.global_config().response_body
-        code = cfg.global_config().response_code
+    def __build_response(self):
+        body = JsonServerHandler.cfg.global_config().response_body
+        code = JsonServerHandler.cfg.global_config().response_code
 
         request_path = self.path
-        for _key in cfg.section_configs().keys():
+        for _key in JsonServerHandler.cfg.section_configs().keys():
             if _key in request_path:
-                target_cfg = cfg.section_configs()[_key]
+                target_cfg = JsonServerHandler.cfg.section_configs()[_key]
                 body_file = open(target_cfg.response_file)
 
                 try:
@@ -55,10 +71,11 @@ class JsonServerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_POST(self):
         self.do_GET()
 
-    def _log_request(self):
+    def __log_request(self):
         SEP = '  '
-        print str(self.command) + SEP + str(self.client_address) + SEP + str(self.path)
+        print SEP.join([str(self.command), str(self.client_address), str(self.path)])
 
 
-httpd = SocketServer.TCPServer((cfg.global_config().host, cfg.global_config().port), JsonServerHandler)
-httpd.serve_forever()
+if __name__ == '__main__':
+    status = main()
+    exit(status)
